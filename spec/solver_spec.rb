e9c86b1d02d8771972c0207b6e4cafe2b32b8bce
Jamie95187 <jamiewong63@googlemail.com>
2020-07-06 21:18:42 +0100

    test acceptance criteria 2

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -22,6 +22,10 @@ describe 'solver' do
       expect(solver.find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).to eq 5
     end
 
+    it('should return -1') do
+      expect(solver.find_it([1,1,2,-2,5,2,4,4,-1,-2,5])).to eq -1
+    end
+
   end
 
 end
